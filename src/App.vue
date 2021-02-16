feca1021aae2fdc4bab5c784567c22ed0cca1b58
NeginSal <neginsaljooghi@gmail.com>
2021-02-16 21:32:54 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+         <appointment-list :appointments="appointments" @remove="removeItem" @edit="editItem"/>
+    </div> 
+  </div>
+</template>
+
+<script>
+
+import AppointmentList from "./components/AppointmentList";
+import _ from "lodash";
+import axios from "axios";
+
+export default {
+  name: 'MainApp',
+  data: function(){
+    return{
+      title:"Appointment List",
+      appointments:[],
+      aptIndex:0
+    };
+  },
+  components: {
+    AppointmentList
+  },
+  mounted(){
+    axios
+    .get("./data/appointments.json")
+    .then(response => (this.appointments = response.data.map(item =>{
+      item.aptId = this.aptIndex;
+      this.aptIndex++;
+      return item
+    })));
+  },
+  methods:{
+     removeItem:function(apt){
+      this.appointments = _.without(this.appointments, apt);
+     },
+     editItem: function(id, field,text) {
+       const aptIndex= _.findIndex(this.appointments, {
+            aptId:id
+       });
+       this.appointments[aptIndex] [field] = text;
+     }
+  }
+};
+</script>
+
+
